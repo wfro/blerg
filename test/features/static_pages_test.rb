dca58ca9aa1dd6de7664c4b75ef826749944ef97
Will Faurot <wfaurot@gmail.com>
2014-11-01 16:29:38 -0600

    Add more feature tests
    These are very subject to change, it's a mostly static site so these
    tests tend to feel trivial, but I'll keep them for now anyways :D

--- test/features/static_pages_test.rb
+++ test/features/static_pages_test.rb
@@ -2,11 +2,17 @@ require_relative 'feature_test_helper'
 require 'capybara'
 
 class StaticPagesTest < FeatureTest
-  def test_navbar_links_go_to_the_right_places
+  def test_navbar_links
     visit '/'
     assert page.has_link? 'home'
     assert page.has_link? 'about'
     assert page.has_link? 'github'
     assert page.has_link? 'twitter'
+  end
+
+  def test_about_page_has_some_stuff
+    visit '/'
+    click_link 'about'
+    assert page.has_css? '.about', with: 'developer'
   end
 end
