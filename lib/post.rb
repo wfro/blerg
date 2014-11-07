e5c1a5bf41ae7382e8835a0d4a47980783880816
Will Faurot <wfaurot@gmail.com>
2014-11-07 01:59:14 -0700

    Add YAML::Store instance to Post class
    JSON was the original plan but it proved quickly to not be the right
    choice, it was too much of a hassle to constantly deal with i/o.
    Switched to YAML, relying on abstractions from the yaml library to read
    and write from a yaml file.

--- lib/post.rb
+++ lib/post.rb
@@ -1,19 +1,29 @@
+require 'yaml/store'
+
 class Post
   attr_reader :title, :tags, :published_at
 
   def initialize(title: '', tags: '', published_at: nil)
     @title = title
-    @tags  = tags.split(', ')
+    @tags  = tags
     set_published_timestamp(published_at)
+  end
+
+  def self.database(file_path = 'manifest')
+    @database ||= YAML::Store.new file_path
+  end
+
+  def database
+    Post.database
   end
 
   private
 
-  # posts shouldn't be valid without a timestamp as we'd have no
-  # way to reliably sort them for the front page
+  # posts shouldn't be valid without a timestamp as that's how they'll
+  # be sorted usually
   def set_published_timestamp(published_at)
     if published_at.nil?
-      raise ArgumentError, 'You must provide a timestamp (Time.now) when creating a post.'
+      raise ArgumentError, 'You must provide a timestamp when creating a post.'
     end
     @published_at = published_at
   end
--- test/post_test.rb
+++ test/post_test.rb
@@ -2,20 +2,33 @@ require_relative 'test_helper'
 require_relative '../lib/post'
 
 class PostTest < Minitest::Test
-  def test_it_saves_attributes_correctly
-    post = Post.new(title: "Breaking: Jon Snow Knows Nothing",
-                    tags: "got, the wall",
-                    published_at: Time.now)
-    assert_equal "Breaking: Jon Snow Knows Nothing", post.title
-    assert_equal ["got", "the wall"], post.tags
-    assert_respond_to post, 'published_at'
+  def setup
+    Post.database 'test/fixtures/manifest'
   end
 
-  def test_not_having_published_at_raises_argument_error
+  def teardown
+    File.open('./test/fixtures/manifest', 'w') { |file| file.truncate(0) }
+  end
+
+  def test_it_saves_attributes_correctly
+    time = Time.now
+    post = Post.new(title: 'Breaking: Jon Snow Knows Nothing',
+                    tags: 'got, the wall',
+                    published_at: time)
+    assert_equal 'Breaking: Jon Snow Knows Nothing', post.title
+    assert_equal 'got, the wall', post.tags
+    assert_equal time, post.published_at
+  end
+
+  def test_not_having_published_at_raises_an_argument_error
     assert_raises(ArgumentError) do
       Post.new(title: "Breaking: Jon Snow Knows Nothing",
                tags: "got, the wall")
     end
+  end
+
+  def test_it_has_a_data_store_instance
+    assert_instance_of Psych::Store, Post.database
   end
 end
 
