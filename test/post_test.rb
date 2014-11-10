080af490f3a6cac3978576e0818166a30eeb7d04
Will Faurot <wfaurot@gmail.com>
2014-11-09 21:37:19 -0700

    Remove file extensions from blog filenames in tests

--- test/post_test.rb
+++ test/post_test.rb
@@ -15,7 +15,7 @@ class PostTest < Minitest::Test
     post = Post.new(title: 'Breaking: Jon Snow Knows Nothing',
                     tags: 'got, the wall',
                     published_at: time,
-                    filename: 'blog_post.md')
+                    filename: 'blog_post')
     assert_equal 'Breaking: Jon Snow Knows Nothing', post.title
     assert_equal 'got, the wall', post.tags
     assert_equal time, post.published_at
@@ -25,7 +25,7 @@ class PostTest < Minitest::Test
     assert_raises(ArgumentError) do
       Post.new(title: "Breaking: Jon Snow Knows Nothing",
                tags: "got, the wall",
-               filename: "blog_post.md")
+               filename: "blog_post")
     end
   end
 
@@ -42,22 +42,22 @@ class PostTest < Minitest::Test
   end
 
   def test_it_saves_a_post_to_the_manifest
-    post = Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post.md')
+    post = Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post')
     post.save
     yaml = YAML.load_file 'test/fixtures/manifest'
     assert_equal 'A title', yaml['posts'].first[:title]
   end
 
   def test_it_has_a_collection_of_post_objects
-    3.times { Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post.md').save }
+    3.times { Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post').save }
     assert_instance_of Array, Post.all
     assert_instance_of Post, Post.all.first
     assert_equal Post.all.first.title, 'A title'
   end
 
   def test_it_returns_the_most_recent_post
-    post = Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post.md').save
-    post = Post.new(title: "Another title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post.md').save
+    post = Post.new(title: "A title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post').save
+    post = Post.new(title: "Another title", tags: "tag1, tag2", published_at: Time.now, filename: 'blog_post').save
     assert_equal "Another title", Post.last.title
   end
 end
