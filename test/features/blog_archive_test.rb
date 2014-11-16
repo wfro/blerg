c5ccdc422ea7c49d92e5f023a19ef3943b961bb4
Will Faurot <wfaurot@gmail.com>
2014-11-15 21:10:17 -0700

    Update archive test

--- test/features/blog_archive_test.rb
+++ test/features/blog_archive_test.rb
@@ -2,6 +2,10 @@ require_relative 'feature_test_helper'
 
 class BlogArchiveTest < FeatureTest
   def test_it_displays_a_list_of_all_posts
+    post = Post.new(filename: "2014-10-27-gems-for-debugging-and-testing", 
+                    published_at: Time.now)
+    post.save
+    
     visit '/'
     click_link 'archive'
     assert page.has_css? '.archive-container'
